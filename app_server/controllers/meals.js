1601642da7403b77b3289f16d71a43c761c4d6ab
Matt Atencio <matthew.atencio@snhu.edu>
2020-11-15 19:44:27 -0500

    Convert meals and rooms templates from static data to dynamic JSONs

--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -1,7 +1,10 @@
+const fs = require('fs');
+const dishes = JSON.parse(fs.readFileSync('./data/dishes.json', 'utf8'));
+
 const meals = (req, res) => {
     //kept getting undefined for description var, so left out for now
     //pageTitle = process.env.npm_package_description + ' - Meals';
-    res.render('meals', {title: "Travlr Getaways - Meals"});
+    res.render('meals', {title: "Travlr Getaways - Meals", dishes});
 }
 
 module.exports = {
--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,7 +1,11 @@
+const fs = require('fs');
+const roomType = JSON.parse(fs.readFileSync('./data/roomtype.json', 'utf8'));
+
+
 const rooms = (req, res) => {
     //kept getting undefined for description var, so left out for now
     //pageTitle = process.env.npm_package_description + ' - Rooms';
-    res.render('rooms', {title: "Travlr Getaways - Rooms"});
+    res.render('rooms', {title: "Travlr Getaways - Rooms", roomType});
 }
 
 module.exports = {
