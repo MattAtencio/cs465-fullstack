65d9107a52b2ad60f3fefc259e300a5e6b2284cb
Matt Atencio <matthew.atencio@snhu.edu>
2020-11-29 21:21:16 -0500

    Update travel view to pull trip data from database

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,13 +1,49 @@
-const fs = require('fs');
-const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+}
 
-const travel = (req, res) => {
-    //kept getting undefined for description var, so left out for now
-    //pageTitle = process.env.npm_package_description + ' - Travel';
-    res.render('travel', {title: "Travlr Getaways - Travel", trips});
+//GET travel list view
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+    };
+    console.info('>> travelController.travelList calling ' + requestOptions.url);
+    request(
+        requestOptions,
+        (err, { statusCode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            renderTravelList(req, res, body);
+        }
+    );
+};
 
+//internal method to render the travel list
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = 'Travlr Getaways - Travel';
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        repsonseBody = [];
+    } else {
+        if (!responseBody.length) {
+            message = 'No trips exist in our database!';
+        }
+    }
+    res.render('travel',
+        {
+                title: pageTitle,
+                trips: responseBody,
+                message
+        }
+    );
 }
 
 module.exports = {
-    travel
+    travelList
 }
