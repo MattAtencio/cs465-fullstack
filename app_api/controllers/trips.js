d24c568ed767791f3681caca51b27019451e6af9
Matt Atencio <matthew.atencio@snhu.edu>
2020-12-12 20:20:59 -0500

    add user register/login apis and authentication for post/put apis

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose'); //.set('debug', true);
-const model = mongoose.model('trips');
+const Trip = mongoose.model('trips');
+const User = mongoose.model('users');
 
 // GET: /trips - list all the trips
 const tripsList = async (req, res) => {
-    model
+    Trip
         .find({}) //empty filter for all
         .exec((err, trips) => {
                 if (!trips) {
@@ -24,7 +25,7 @@ const tripsList = async (req, res) => {
 
 // GET: /trips/:tripCode - returns a single trip
 const tripsFindByCode = async (req, res) => {
-    model
+    Trip
         .find({'code': req.params.tripCode }) 
         .exec((err, trip) => {
                 if (!trip) {
@@ -45,34 +46,40 @@ const tripsFindByCode = async (req, res) => {
 
 //POST: /trips - add new trip
 const tripsAddTrip = async (req, res) => {
-    model.create({
-        code: req.body.code,
-        name: req.body.name,
-        length: req.body.length,
-        start: req.body.start,
-        resort: req.body.resort,
-        perPerson: req.body.perPerson,
-        image: req.body.image,
-        description: req.body.description
-    },
-    (err, trip) => {
-        if (err) {
-            return res
-                .status(400) // bad request, invalid content
-                .json(err);
-        }
-        else {
-            return res
-                .status(201) // created
-                .json(trip);
-        }
-    });
+    getUser(req, res,
+        (req, res) => {
+            Trip
+                .create({
+                    code: req.body.code,
+                    name: req.body.name,
+                    length: req.body.length,
+                    start: req.body.start,
+                    resort: req.body.resort,
+                    perPerson: req.body.perPerson,
+                    image: req.body.image,
+                    description: req.body.description
+                },
+                    (err, trip) => {
+                        if (err) {
+                            return res
+                                .status(400) // bad request, invalid content
+                                .json(err);
+                        }
+                        else {
+                            return res
+                                .status(201) // created
+                                .json(trip);
+                        }
+                    });
+        })
 }
 
 //POST: /trips - edit existing trip
 const tripsUpdateTrip = async(req, res) => {
-	console.log(req.body);
-	model
+    console.log(req.body);
+    getUser(req, res,
+        (req, res) => {
+	Trip
 		.findOneAndUpdate({	'code': req.params.tripCode	}, {
 			code: req.body.code,
 			name: req.body.name,
@@ -103,8 +110,33 @@ const tripsUpdateTrip = async(req, res) => {
 			return res
 				.status(500) // server error
 				.json(err);
-		});
+        });
+    })
 }
+
+const getUser = (req, res, callback) => {
+    if (req.payload && req.payload.email) {            
+      User
+        .findOne({ email : req.payload.email })         
+        .exec((err, user) => {
+          if (!user) {
+            return res
+              .status(404)
+              .json({"message": "User not found"});
+          } else if (err) {
+            console.log(err);
+            return res
+              .status(404)
+              .json(err);
+           }
+          callback(req, res, user.name);                
+         });
+    } else {
+      return res
+        .status(404)
+        .json({"message": "User not found"});
+    }
+  };
 
 module.exports = {
     tripsList,
