aabcc170eb90adabaf19ebe6bd60b8f821ac92cf
Matt Atencio <matthew.atencio@snhu.edu>
2021-01-28 08:11:32 -0500

    updated News page to load data from json

--- app_server/controllers/news.js
+++ app_server/controllers/news.js
@@ -1,7 +1,10 @@
+const fs = require('fs');
+const blogPosts = JSON.parse(fs.readFileSync('./data/blogPosts.json', 'utf8'));
+
 const news = (req, res) => {
     //kept getting undefined for description var, so left out for now
     //pageTitle = process.env.npm_package_description + ' - News';
-    res.render('news', {title: "Travlr Getaways - News"});
+    res.render('news', {title: "Travlr Getaways - News", blogPosts});
 }
 
 module.exports = {
