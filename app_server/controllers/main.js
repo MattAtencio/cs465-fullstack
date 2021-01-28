e345d388b780ea14e4d5ce3f76e99f8be1ae30ca
Matt Atencio <matthew.atencio@snhu.edu>
2021-01-28 08:11:53 -0500

    Update index to load data from jsons

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
+const blogPosts = JSON.parse(fs.readFileSync('./data/blogPosts.json', 'utf8'));
+const testimonials = JSON.parse(fs.readFileSync('./data/testimonials.json', 'utf8'));
+
 const index = (req, res) => {
-    res.render('index.hbs', {title: 'Travlr Getaways'});
+    res.render('index.hbs', {title: 'Travlr Getaways', blogPosts, testimonials});
 }
 
 module.exports = {
